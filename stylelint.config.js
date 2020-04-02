a2ccc090604445b3dc0ba7dec5b6b285fab9822f
Joan Drago <jdrago10@gmail.com>
2020-04-02 16:58:46 +0200

    stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,19 +2,21 @@ module.exports = {
   extends: "stylelint-config-standard",
   plugins: ["stylelint-scss"],
   rules: {
-    "selector-nested-pattern": "^&",
-    "selector-class-pattern":
-      "^.[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$",
-    "unit-whitelist": ["%", "px", "em", "deg"],
-    indentation: 2,
-    "no-descending-specificity": null,
-    "no-eol-whitespace": null,
-    "declaration-empty-line-before": null,
-    "value-keyword-case": null,
+    // "selector-nested-pattern": "^&",
+    "selector-class-pattern": "[a-z][a-z0-9]*(-[a-z0-9]+)*",
+    "scss/selector-no-redundant-nesting-selector": true,
+    "unit-whitelist": ["%", "px", "em", "deg", "s"],
     "at-rule-no-unknown": [
       true,
       {
-        ignoreAtRules: ["apply", "responsive", "variants", "screen", "use"]
+        ignoreAtRules: [
+          "apply",
+          "sass",
+          "responsive",
+          "variants",
+          "screen",
+          "use"
+        ]
       }
     ]
   }
